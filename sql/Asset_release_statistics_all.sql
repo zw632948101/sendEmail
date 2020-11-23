eb6ff67d17a5dbf132b66481d197a1d6f71b55c1
张伟 <wei.zhang@worldfarm.com>
2020-11-23 13:47:06 +0800

    修改文案

--- sql/Asset_release_statistics_all.sql
+++ sql/Asset_release_statistics_all.sql
@@ -1,5 +1,5 @@
 /*
-{"email_title":"资产发放统计","statement_title":"每日资产发放统计","combine_label":"AssetReleaseStatisticsAll","combine":False,"combine_key":None,"DBname":"mp","DBstatus":True,"DBlist":[{"DBname":"flower","sqlfile":"creator_name.sql","db_key":"操作人","replace":"creator_id"}]}
+{"email_title":"资产发放统计","statement_title":"历史资产发放统计","combine_label":"AssetReleaseStatisticsAll","combine":False,"combine_key":None,"DBname":"mp","DBstatus":True,"DBlist":[{"DBname":"flower","sqlfile":"creator_name.sql","db_key":"操作人","replace":"creator_id"}]}
 */
 SELECT date_format(b.create_time, '%Y-%m-%d') AS '合作时间', sum(b.number) AS '发放数量/箱', ba.`name` AS '蜂友姓名', ba.phone AS '蜂友电话', b.creator_id AS '操作人'
 FROM `mp-asset`.t_asset_ledger_batch b,
