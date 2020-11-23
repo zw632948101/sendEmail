63b2c1e56a36425174aeab5add1164bdf0cc716f
张伟 <wei.zhang@worldfarm.com>
2020-11-23 15:27:15 +0800

    增加资产发放统计蜂场位置

---
+++ sql/substatements/asset_release_apiary_info.sql
@@ -0,0 +1,3 @@
+SELECT ts.id AS '蜂场id', concat((SELECT tr.name FROM `fc-bee`.t_region tr WHERE tr.id = ts.province), '/', (SELECT tr.name FROM `fc-bee`.t_region tr WHERE tr.id = ts.city), '/', (SELECT tr.name FROM `fc-bee`.t_region tr WHERE tr.id = ts.county)) AS '蜂场位置'
+FROM `fc-bee`.t_swarm_info ts
+WHERE ts.id IN swarmid;
