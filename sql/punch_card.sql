7ef76fc2329063fd3779a11a17e7d2f4dea79246
heshichao <463494980@qq.com>
2020-12-17 14:23:46 +0800

    上下班打卡sql修改

--- sql/punch_card.sql
+++ sql/punch_card.sql
@@ -1,40 +1,19 @@
 /*
 {"email_title":"追花族上下班打卡","statement_title":"每日上下班打卡统计","combine_label":"ClockInAndOutStatistics","combine":False,"combine_key":None}
 */
-SELECT a.real_name      AS                                                                                           '姓名',
-       CASE tu.role_code WHEN 1004 THEN '养蜂助理' WHEN 1005 THEN '项目经理' WHEN 1006 THEN '项目专员' WHEN 1007 THEN '后端运营' END '角色',
-       a.contact_number AS                                                                                           '手机号',
-       a.create_time    AS                                                                                           '上班打卡',
-       b.endtime        AS                                                                                           '下班打卡',
-       a.lng,
-       a.lat,
-       a.address        AS                                                                                           '打卡地址'
-FROM (SELECT t.real_name,
-             t.contact_number,
-             CASE s.type
-                 WHEN 2 THEN
-                     '上班打卡'
-                 WHEN 3 THEN
-                     '下班打卡'
-                 ELSE
-                     '普通定位'
-                 END 'typeStr',
-             s.create_time,
-             s.user_id,
-             s.lng,
-             s.lat,
-             s.address
-      FROM `fc-bee`.t_bee_friend t,
-           `fc-bee`.t_staff_trail s
-      WHERE t.user_id = s.user_id
-        AND s.type = 2
-        AND TO_DAYS(s.create_time) = TO_DAYS(NOW())) a
-         LEFT OUTER JOIN
-     (SELECT user_id, max(create_time) AS endtime
-      FROM `fc-bee`.t_staff_trail
-      WHERE type = 3
-        AND TO_DAYS(create_time) = TO_DAYS(NOW()) - 1
-      GROUP BY user_id) b
-     ON a.user_id = b.user_id
-         LEFT OUTER JOIN `fc-bee`.t_user_role tu ON a.user_id = tu.user_id AND tu.is_delete = 0 AND tu.role_code IN (1004, 1005, 1006, 1007)
-ORDER BY a.create_time DESC;
+SELECT user_all1.real_name as '姓名',user_all1.value as '角色',user_all1.contact_number as '手机号',user_all1.lng,user_all1.lat,
+user_all1.shangbandaka as '上班打卡',before_ka1.xiabandaka as '下班打卡',user_all1.address as '打卡地址' FROM
+(SELECT user_all.*,before_ka.* FROM
+(SELECT f.real_name,f.contact_number,c.`value`,f.user_id
+FROM `fc-bee`.t_bee_friend f,`fc-bee`.t_user_role r,`fc-bee`.t_config c
+WHERE r.user_id=f.user_id AND c.`key`=r.role_code AND c.`code`=10001 AND r.role_code IN (1007,1005,1006)
+AND f.is_delete=0 AND r.is_delete=0 AND c.is_delete=0) user_all LEFT OUTER JOIN
+(SELECT user_id as userID,lng,lat,create_time as shangbandaka,address
+FROM `fc-bee`.t_staff_trail
+WHERE TO_DAYS(create_time) = TO_DAYS(NOW()) AND type=2 AND is_delete=0) before_ka
+ON before_ka.userID=user_all.user_id) user_all1 LEFT OUTER JOIN
+(SELECT user_id as user_ID,max(create_time) as xiabandaka
+FROM `fc-bee`.t_staff_trail
+WHERE TO_DAYS(create_time) = TO_DAYS(NOW())-1 AND type=3 AND is_delete=0
+GROUP BY user_id) before_ka1
+ON before_ka1.user_ID=user_all1.user_id;
