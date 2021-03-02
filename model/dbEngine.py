b85616b49a382520aff67ef5ec3093d924dbaf19
张伟 <wei.zhang@worldfarm.com>
2021-03-02 16:43:34 +0800

    重构发送邮箱项目

--- model/dbEngine.py
+++ model/dbEngine.py
@@ -5,15 +5,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from fileDir.fileDir import CONFIG_DB
 from model import Base
+from pathlib import Path
+import os
 
 
 class DBEngine(object):
     def __init__(self):
         # 初始化数据库链接
-        # case_path = os.path.dirname(__file__)
+        if not Path(CONFIG_DB).is_dir():
+            os.mkdir(CONFIG_DB)
         self.dataBaseURI = f'sqlite:///{CONFIG_DB}/sendEmail.db'
-        print(self.dataBaseURI)
-        # self.dataBaseURI = 'mysql+pymysql://jira:jirapasswd@132.232.46.177:3306/jira'
         self.__engine = create_engine(self.dataBaseURI, echo=False)
         Base.metadata.create_all(self.__engine)
         self.session = None
