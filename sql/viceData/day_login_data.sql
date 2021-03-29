3251c0e8660a222f8b374cfef87d2261cbb34bdb
baron.zhang <632948101@qq.com>
2021-03-29 20:04:09 +0800

    Delete sql/viceData directory

--- sql/viceData/day_login_data.sql
+++
@@ -1,6 +0,0 @@
--- 每日采集蜂友统计 - 每日登陆数据
-SELECT tlt.user_id
-FROM `world-passport`.t_login_token tlt
-         LEFT JOIN `world-user`.t_user tu ON tlt.user_id = tu.id AND tu.account_type = 21
-WHERE to_days(tlt.create_time) = to_days(now())
-GROUP BY tlt.user_id;
