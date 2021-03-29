e22a0ba65bdb27990dbe30f980fa81344447e302
baron.zhang <632948101@qq.com>
2021-03-29 20:03:56 +0800

    Delete sql/busuness directory
    shanchu

--- sql/busuness/creator_name.sql
+++
@@ -1,4 +0,0 @@
--- 资产发放统计 - 操作人查询
-SELECT tb.user_id AS '操作人', tb.real_name AS '操作人姓名'
-FROM `fc-bee`.t_bee_friend tb
-WHERE tb.user_id IN 操作人list;
--- sql/busuness/query_username.sql
+++
@@ -1,2 +0,0 @@
-SELECT tbf.user_id,tbf.real_name AS '创建人'
-FROM `fc-bee`.t_bee_friend tbf WHERE tbf.is_delete = 0 AND tbf.user_id IN userid;
